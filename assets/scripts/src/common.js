ac6f8c70156187e452c958a0d78db9dc40721c89
Denis Ziegler <dzincolorado@yahoo.com>
2012-08-16 00:58:03 -0600

    dynamic sliders

--- assets/scripts/src/common.js
+++ assets/scripts/src/common.js
@@ -1,21 +1,48 @@
+var trackerId = 0; //TODO: depracate in favor of a storage driven Id.
 
-//setup event handlers
-$(document).ready(function(){
+function Tracker(id, startMinute, endMinute, activity){
+	var self = this;
+	
+	self.trackerId = id;
+	self.startMinute = ko.observable(startMinute);
+	self.endMinute = ko.observable(endMinute);
+	self.activity = ko.observable(activity);
+}
+
+function TrackerViewModel(){
+	var self = this;
+	
+	self.trackers = ko.observableArray([]);
+	
+	self.addTracker = function(id, startMinute, endMinute, activity){
+		self.trackers.push(new Tracker(id, startMinute, endMinute, activity));
+		
+		//TODO:  need to reset activity and slider values
+	}
+}
+
+function ConfigureTracker(id, startMinute, endMinute){
 	$(function() {
-		$( "#slider-range" ).slider({
+		var sliderRangeId ="#slider-range" + id; 
+		var txtMinutesId ="#txtMinutes" + id;
+		//alert($( sliderRangeId ).slider( "values", 1 ));
+		 
+		$( sliderRangeId ).slider({
 			range: true,
 			min: 0,
 			max: 288,
 			step: 5,
-			values: [ 0, 5 ],
+			values: [ startMinute, endMinute ],
 			slide: function( event, ui ) {
-				$( "#minutes" ).val(ui.values[ 1 ] - ui.values[ 0 ] + " minutes" );
+				$( txtMinutesId ).val(ui.values[ 1 ] - ui.values[ 0 ] + " minutes" );
 			}
 		});
-		$( "#minutes" ).val($( "#slider-range" ).slider( "values", 1 ) - 
-			$( "#slider-range" ).slider( "values", 0 ) + " minutes");
+		$( txtMinutesId ).val($( sliderRangeId ).slider( "values", 1 ) - 
+			$( sliderRangeId ).slider( "values", 0 ) + " minutes");
 	});
-	
+}
+
+function ConfigureTrackAnotherButton(){
 	//configure fx speeds
 	$.fx.speeds._default = 300;
 	$(function() {
@@ -30,4 +57,62 @@ $(document).ready(function(){
 			return false;
 		});
 	});
+}
+
+function ConfigureTrackNewButton(){
+	$( "#btnTrackNew" ).click(function() {
+		
+		var startMinute = $("#slider-range" ).slider("values", 0 );
+		var endMinute = $("#slider-range" ).slider("values", 1 );
+		var activity = $("#txtActivity").val();
+		
+		trackerId++;
+		model.addTracker(trackerId, startMinute, endMinute, activity);
+		ConfigureTracker(trackerId, startMinute, endMinute);
+		
+		$( "#wrpTrackAnother" ).dialog( "close" );
+		return false;
+	});
+}
+
+function ConfigureDataBindings(){
+	ko.applyBindings(model);
+}
+
+function ConfigureAutoComplete(){
+	//configure autocomplete
+	$(function() {
+		var cache = {},
+			lastXhr;
+		$( "#txtActivity" ).autocomplete({
+			minLength: 2,
+			source: function( request, response ) {
+				var term = request.term;
+				if ( term in cache ) {
+					response( cache[ term ] );
+					return;
+				}
+
+				lastXhr = $.getJSON( "autocomplete", request, function( data, status, xhr ) {
+					cache[ term ] = data;
+					if ( xhr === lastXhr ) {
+						response( data );
+					}
+				});
+			}
+		});
+	});
+}
+
+
+var model = new TrackerViewModel();
+//setup event handlers
+$(document).ready(function(){
+	
+	ConfigureDataBindings();
+	ConfigureTracker("", 0, 5);
+	ConfigureTrackAnotherButton();
+	ConfigureTrackNewButton();
+	ConfigureAutoComplete();
+	
 })
--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,14 @@
 module.exports = function(expressServer, passport, routeCallbacks){
 	expressServer.get("/", function(request, response){
 		response.render("trackers", {locals: {}});
+	});
+	
+	//TODO:get values from store and also save new value if applicable
+	expressServer.get("/autocomplete", function(request, response){
+		response.writeHead(200, {"content-type":"text/plain"});
+		
+		var suggestedValues = ["test1", "test2"];
+		response.write(JSON.stringify(suggestedValues));
+		response.end();
 	})
 }	
