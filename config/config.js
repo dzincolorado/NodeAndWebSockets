564caf2b6853221f8f79db8a2d4715f04e4b0fc0
Denis Ziegler <dzincolorado@yahoo.com>
2012-11-12 21:50:15 -0700

    consumer key update

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@
 module.exports = {
 	facebook: {
 		consumerKey: "376509982436950",
-		consumerSecret: "e6c6cbae972859645e26e68af2043944",
+		consumerSecret: "8edbdb260256e2fb7e68835a0a154469",
 		callbackURL: "http://127.0.0.1:8000/auth/facebook/callback"
 	}
 }
