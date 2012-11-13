dbf03ff88f5908b8e1f7a67c0a566ede14258487
Denis Ziegler <dzincolorado@yahoo.com>
2012-11-12 20:53:47 -0700

    testing fb

--- assets/scripts/src/facebook.js
+++ assets/scripts/src/facebook.js
@@ -1,7 +1,23 @@
+/*
+// Additional JS functions here
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '376509982436950', // App ID
+      channelUrl : '//nodeandwebsockets.herokuapp.com/channel.html', // Channel File
+      status     : true, // check login status
+      cookie     : true, // enable cookies to allow the server to access the session
+      xfbml      : true  // parse XFBML
+    });
+
+    // Additional init code here
+
+  };
+*/
+
 (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=APP_ID";
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=376509982436950";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 //TODO: store key and secret in DB
 module.exports = {
 	facebook: {
-		consumerKey: "",
-		consumerSecret: "",
+		consumerKey: "376509982436950",
+		consumerSecret: "e6c6cbae972859645e26e68af2043944",
 		callbackURL: "http://127.0.0.1:8000/auth/facebook/callback"
 	}
 }
