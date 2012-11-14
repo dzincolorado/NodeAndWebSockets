1deed3862ede0ef72658f1cc31ece12cc1108fbe
Denis Ziegler <dzincolorado@yahoo.com>
2012-11-13 23:43:08 -0700

    clean up

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@
 module.exports = {
 	facebook: {
 		consumerKey: "376509982436950",
-		consumerSecret: "8edbdb260256e2fb7e68835a0a154469",
+		consumerSecret: "",
 		callbackURL: "http://nodeandwebsockets.herokuapp.com/auth/facebook/callback"
 	}
 }
